16863f2034637e92d04f19e7b067f040f54163f2
Edenedry1 <117269968+Edenedry1@users.noreply.github.com>
2024-07-16 17:49:48 +0200

    Login html

--- ReqGenius/ReqGenius/views.py
+++ ReqGenius/ReqGenius/views.py
@@ -6,3 +6,6 @@ def HomePage(request):
 def about(request):
     # return HttpResponse("My about")
     return render(request, 'about.html')
+def Login(request):
+    # return HttpResponse("Login")
+    return render(request, 'Login.html')

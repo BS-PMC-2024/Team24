04a5d985ec91ccea46e1372bf6d5e6215e39dd6c
noaro2 <117273562+noaro2@users.noreply.github.com>
2024-07-23 17:32:43 +0300

    Update views.py

--- ReqGenius/ReqGenius/views.py
+++ ReqGenius/ReqGenius/views.py
@@ -1,17 +1,13 @@
-# from django.http import HttpResponse
 from django.shortcuts import render
+
 def HomePage(request):
-    # return HttpResponse("Hello")
     return render(request, 'Home.html')
+
 def about(request):
-    # return HttpResponse("My about")
     return render(request, 'about.html')
-<<<<<<< HEAD
+
 def signup(request):
-    # return HttpResponse("My about")
     return render(request, 'signup.html')
-=======
+
 def Login(request):
-    # return HttpResponse("Login")
     return render(request, 'Login.html')
->>>>>>> 16863f2034637e92d04f19e7b067f040f54163f2

e61a6ac333d76018928f62cb19024209435bd032
noaro2 <noarofe2@gmail.com>
2024-07-30 14:32:31 +0300

    update test

--- ReqGenius/test.py
+++ ReqGenius/test.py
@@ -1,61 +1,24 @@
-from django.test import SimpleTestCase
-from django.urls import reverse, resolve
-# from django.contrib import admin
-from ReqGenius.views import HomePage, signup, Login
-
-class UrlTests(SimpleTestCase):
-    #     url = reverse('admin:index') test!
-
-    def test_home_url_resolves_home_page_view(self):
-        url = reverse('home')
-        self.assertEqual(resolve(url).func, HomePage)
-
-   
-
-    def test_signup_url_resolves_signup_view(self):
-        url = reverse('signup')
-        self.assertEqual(resolve(url).func, signup)
-
-    def test_login_url_resolves_login_view(self):
-        url = reverse('login')
-        self.assertEqual(resolve(url).func, Login)
-from django.test import TestCase, Client
+from django.test import TestCase
 from django.urls import reverse
 
-class HomeViewTests(TestCase):
-    def setUp(self):
-        self.client = Client()
+class ViewsTestCase(TestCase):
 
-    def test_home_view_status_code(self):
-        response = self.client.get(reverse('home'))
+    def test_home_page_view(self):
+        response = self.client.get(reverse('HomePage'))
         self.assertEqual(response.status_code, 200)
-
-    def test_home_view_template(self):
-        response = self.client.get(reverse('home'))
         self.assertTemplateUsed(response, 'Home.html')
 
+    # def test_about_view(self):
+    #     response = self.client.get(reverse('about'))
+    #     self.assertEqual(response.status_code, 200)
+    #     self.assertTemplateUsed(response, 'about.html')
 
-
-class SignupViewTests(TestCase):
-    def setUp(self):
-        self.client = Client()
-
-    def test_signup_view_status_code(self):
+    def test_signup_view(self):
         response = self.client.get(reverse('signup'))
         self.assertEqual(response.status_code, 200)
-
-    def test_signup_view_template(self):
-        response = self.client.get(reverse('signup'))
         self.assertTemplateUsed(response, 'signup.html')
 
-class LoginViewTests(TestCase):
-    def setUp(self):
-        self.client = Client()
-
-    def test_login_view_status_code(self):
-        response = self.client.get(reverse('login'))
+    def test_login_view(self):
+        response = self.client.get(reverse('Login'))
         self.assertEqual(response.status_code, 200)
-
-    def test_login_view_template(self):
-        response = self.client.get(reverse('login'))
         self.assertTemplateUsed(response, 'Login.html')

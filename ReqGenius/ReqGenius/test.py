3359b6750b78d84b41251e568a6b7fa5480b854f
noaro2 <noarofe2@gmail.com>
2024-07-30 15:29:58 +0300

    update

--- ReqGenius/ReqGenius/test.py
+++ ReqGenius/ReqGenius/test.py
@@ -1,5 +1,11 @@
 from django.test import TestCase
 from django.urls import reverse
+from django.test import TestCase
+
+class SimpleTest(TestCase):
+    def test_addition(self):
+        self.assertEqual(1 + 1, 2)
+
 
 class ViewsTestCase(TestCase):
 

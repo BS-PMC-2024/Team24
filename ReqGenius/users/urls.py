dba25df7e5f33f3acb6b6f9cd3982fc981da63e2
Yovelda <117283925+Yovelda@users.noreply.github.com>
2024-08-20 22:24:04 +0300

    update

--- ReqGenius/users/urls.py
+++ ReqGenius/users/urls.py
@@ -2,23 +2,31 @@
 
 from django.urls import path
 from . import views
+from django.urls import path, include
+
+
 
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.home, name='hometest'),
     path('signup/', views.signup, name='signup'),
     path('login/', views.login, name='login'),
     path('support/', views.support, name='support'),
     path('feedback/', views.feedback, name='feedback'),
-    path('feedback.html', views.feedback, name='feedback_html'),
-    path('srsinfo/', views.srsinfo, name='srsinfo'),
-    path('srsinfo.html', views.srsinfo, name='srsinfo_html'),
-    path('err/', views.err, name='err'),
-    path('err.html', views.err, name='err_html'),
-    path('srs/', views.srs, name='srs'),
+    path('srsnew/', views.srsnew, name='srsnew'),
     path('about/', views.about, name='about'),
-    path('student-dashboard/', views.dashStudent, name='dashStudent'),
-    path('client-dashboard/', views.dashClient, name='dashClient'),
+    path('adminhome/', views.adminhome, name='adminhome'),
+    path('want/', views.want, name='want'),
+    path('admin_feedback/', views.admin_feedback, name='admin_feedback'),
+    path('support-request/', views.support_request, name='support_request'),
+    path('srs_students/', views.srs_students, name='srs_students'),
+    path('admin_manage_content/', views.add_educational_content, name='admin_manage_content'),
+    path('remove_content/<int:content_id>/', views.remove_educational_content, name='remove_content'),
+    path('view_content/', views.view_educational_content, name='view_content'),
+    path('add_zoom_meeting/', views.add_zoom_meeting, name='add_zoom_meeting'),
+    path('remove_zoom_meeting/<int:meeting_id>/', views.remove_zoom_meeting, name='remove_zoom_meeting'),
+
+   
 
 ]
 
